44255d1792e6e999950957b2e061795a9307e4ef
Vishal-pandey17 <vishalpandey86681@gmail.com>
2025-01-13 17:18:32 +0530

    Display Names completed successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { use, useState } from "react";
 
 function App() {
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
+   const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFullName(`${firstName} ${lastName}`);
+  }
   return (
     <div>
-      Hello pandey!
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <label>First Name:</label>
+        <input type="text" id="fname" value={firstName} onChange={(e) => setFirstName(e.target.value)}/><br/>
+        <label>Last Name:</label>
+        <input type="text" id="lname" value={lastName} onChange={(e) => setLastName(e.target.value)}/><br/>
+        <button type="submit">Submit</button>
+      </form>
+      {fullName && (
+        <div>
+          <label>Full Name:<span>{fullName}</span></label>
+          
+        </div>
+      )}
     </div>
   );
 }
